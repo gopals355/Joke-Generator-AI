bb5477a7c7d54a5ac797f9986a294a77c2e41511
gopals355 <128239441+gopals355@users.noreply.github.com>
2023-03-19 05:44:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 # Load the data from each file
-data1 = pd.read_excel('jester-data-1.xls', header=None)
-data2 = pd.read_excel('jester-data-2.xls', header=None)
-data3 = pd.read_excel('jester-data-3.xls', header=None)
+data1 = pd.read_excel('./jester-data/jester-data-1.xls', header=None)
+data2 = pd.read_excel('./jester-data/jester-data-2.xls', header=None)
+data3 = pd.read_excel('./jester-data/jester-data-3.xls', header=None)
 
 # Combine the data into a single dataset
 data = pd.concat([data1, data2, data3])
